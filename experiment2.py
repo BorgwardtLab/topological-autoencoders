05e9b07d2f2a20c3bb358e530067fa6add095c23
Max Horn <max.horn@bsse.ethz.ch>
2019-04-25 17:38:16 +0200

    Moved topological AE into single module

--- experiment2.py
+++ experiment2.py
@@ -59,15 +59,17 @@ class TopologicalSignature(nn.Module):
         self.p = p
         self.signature_calculator = PersistentHomologyCalculation()
 
-    def forward(self, x):
+    def forward(self, x, norm=False):
         """Take a batch of instances and return the topological signature."""
         distances = torch.norm(x[:, None] - x, dim=2, p=self.p)
+        if norm:
+            distances = distances / distances.max()
         pairs = self.signature_calculator(distances.detach().numpy())
         selected_distances = distances[(pairs[:, 0], pairs[:, 1])]
         return selected_distances
 
 
-class TopologicalRegularization(nn.Module):
+class SignatureDistance(nn.Module):
     def __init__(self, p=2):
         super().__init__()
         self.p = p
@@ -88,12 +90,33 @@ class TopologicalRegularization(nn.Module):
         return self._pers_dist(x1, x2)
 
 
+class TopoRegAutoencoder(nn.Module):
+    def __init__(self, lam=1.):
+        super().__init__()
+        self.lam = lam
+        self.autoencoder = Autoencoder()
+        self.topo_sig = TopologicalSignature()
+        self.sig_error = SignatureDistance()
+        self.reconst_error = nn.MSELoss()
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        latent, reconst = self.autoencoder(x)
+        sig_data = self.topo_sig(x.view(batch_size, -1), norm=True)
+        sig_latent = self.topo_sig(latent)
+
+        reconst_error = self.reconst_error(x, reconst)
+        topo_error = self.sig_error(sig_data, sig_latent)
+        return (
+            reconst_error + self.lam * topo_error,
+            (reconst_error, topo_error)
+        )
+
+
 def main():
     num_epochs = 10
     batch_size = 32
     learning_rate = 1e-3
-    lam1 = 1.
-    lam2 = 1.
 
     img_transform = transforms.Compose([
         transforms.ToTensor(),
@@ -103,32 +126,17 @@ def main():
     dataset = MNIST('./data', transform=img_transform, download=True)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    model = Autoencoder() #.cuda()
-    topo_sig = TopologicalSignature()
-    reg = TopologicalRegularization()
-    criterion = nn.MSELoss()
+    model = TopoRegAutoencoder()
     optimizer = torch.optim.Adam(
-        chain(model.parameters(), reg.parameters()),
-        lr=learning_rate, weight_decay=1e-5
-    )
+        model.parameters(), lr=learning_rate, weight_decay=1e-5)
 
     for epoch in range(num_epochs):
         for i, data in enumerate(dataloader):
             img, _ = data
             img = Variable(img)  #.cuda()
-            bs = img.size()[0]
 
             # Autoencoder
-            latent, reconstructed = model(img)
-            reconst_error = criterion(reconstructed, img)
-
-            # Topological signatures
-            topo_sig_data = topo_sig(img.view(bs, -1))
-            topo_sig_latent = topo_sig(latent.view(bs, -1))
-
-            # Regularization
-            topo_reg = reg(topo_sig_data, topo_sig_latent)
-            loss = reconst_error + lam1*topo_reg
+            loss, (reconst_error, topo_error) = model(img)
 
             optimizer.zero_grad()
             loss.backward()
@@ -136,12 +144,13 @@ def main():
             if i % 10 == 0:
                 print(
                     f'MSE: {reconst_error}, '
-                    f'topo_reg: {topo_reg}'
+                    f'topo_reg: {topo_error}'
                 )
         # ===================log========================
         print('epoch [{}/{}], loss:{:.4f}'
               .format(epoch+1, num_epochs, loss.data.item() )) #loss.data[0] 
         if epoch % 1 == 0:
+            _, reconstructed = model.autoencoder(img)
             pic = to_img(reconstructed.cpu().data)
             save_image(pic, './dc_img/image_{}.png'.format(epoch))
 
