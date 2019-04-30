b69d025646abb150770506857877e430a0bac18f
Max Horn <max.horn@bsse.ethz.ch>
2019-04-30 21:12:37 +0200

    Added documentation to callbacks

--- src/callbacks.py
+++ src/callbacks.py
@@ -3,6 +3,11 @@ import os
 from tqdm import tqdm
 from torchvision.utils import save_image
 
+# Hush the linter, child callbacks will always have different parameters than
+# the overwritten method of the parent class.
+# pylint: disable=W0221,W0613
+
+
 class Callback():
     """Callback for training loop."""
 
@@ -20,12 +25,21 @@ class Callback():
 
 
 class Progressbar(Callback):
+    """Callback to show a progressbar of the training progress."""
+
     def __init__(self, print_loss_components=False):
+        """Show a progressbar of the training progress.
+
+        Args:
+            print_loss_components: Print all components of the loss in the
+                progressbar
+        """
         self.print_loss_components = print_loss_components
         self.total_progress = None
         self.epoch_progress = None
 
     def on_epoch_begin(self, n_epochs, n_instances, **kwargs):
+        """Initialize the progressbar."""
         if self.total_progress is None:
             self.total_progress = tqdm(
                 position=0, total=n_epochs, unit='epochs')
@@ -33,6 +47,7 @@ class Progressbar(Callback):
             position=1, total=n_instances, unit='instances')
 
     def on_batch_end(self, batch_size, loss, loss_components, **kwargs):
+        """Increment progressbar and update description."""
         self.epoch_progress.update(batch_size)
         description = f'Loss: {loss:3.3f}'
         if self.print_loss_components:
@@ -44,6 +59,7 @@ class Progressbar(Callback):
         self.epoch_progress.set_description(description)
 
     def on_epoch_end(self, epoch, n_epochs, **kwargs):
+        """Increment total training progressbar."""
         self.epoch_progress.close()
         self.epoch_progress = None
         self.total_progress.update(1)
@@ -52,13 +68,20 @@ class Progressbar(Callback):
 
 
 class SaveReconstructedImages(Callback):
+    """Callback to save images of the reconstruction."""
+
     def __init__(self, path):
+        """Save images of the reconstruction.
+
+        Args:
+            path: Path to store the images to
+        """
         self.path = path
 
     def on_epoch_end(self, model, dataset, img, epoch, **kwargs):
+        """Save reconstruction images."""
         latent = model.encode(img)
         reconst = model.decode(latent)
         reconstructed_image = dataset.inverse_normalization(reconst)
         save_image(
-            reconstructed_image, os.path.join(self.path,f'epoch_{epoch}.png'))
-
+            reconstructed_image, os.path.join(self.path, f'epoch_{epoch}.png'))
