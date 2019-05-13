82aca8ad36ef22923adf98365b8f8fecce8e62f5
Max Horn <max.horn@bsse.ethz.ch>
2019-05-13 12:18:49 +0200

    Use predefined test splits from datasets, evaluation in callbacks
     - use train keyword to define if testing is used
     - moved evaluation on validation and testing to separate callback

--- exp/callbacks.py
+++ exp/callbacks.py
@@ -2,6 +2,7 @@
 from collections import defaultdict
 
 import numpy as np
+from torch.utils.data import DataLoader
 
 from src.callbacks import Callback
 
@@ -46,3 +47,63 @@ class LogTrainingLoss(Callback):
             self.run.log_scalar(key + '.std', std, self.iterations)
             self.logged_stds[key].append(std)
         self.epoch_losses = defaultdict(list)
+
+
+class LogDatasetLoss(Callback):
+    """Logging of loss during training into sacred run."""
+
+    def __init__(self, dataset_name, dataset, run, batch_size=128,
+                 frequency=4):
+        """Create logger callback.
+
+        Log the training loss using the sacred metrics API.
+
+        Args:
+            run: Sacred run
+        """
+        self.prefix = dataset_name
+        self.dataset = dataset
+        self.data_loader = DataLoader(self.dataset, batch_size=batch_size,
+                                      drop_last=False)
+        self.run = run
+        self.iterations = 0
+
+    def _compute_average_losses(self, model):
+        losses = defaultdict(list)
+
+        for batch in self.data_loader:
+            data, _ = batch
+            loss, loss_components = model(data)
+
+            # Rescale the losses as batch_size might not divide dataset
+            # perfectly
+            n_instances = len(data)
+            losses[f'{self.prefix}.loss'].append(loss.item()*n_instances)
+            for loss_component, value in loss_components.items():
+                losses[f'{self.prefix}.{loss_component}'].append(
+                    value.item()*n_instances)
+        return {
+            name: sum(values) / len(self.dataset)
+            for name, values in losses.items()
+        }
+
+    def on_epoch_begin(self, model, epoch, **kwargs):
+        """Store the loss on the dataset prior to training."""
+        if epoch == 1:  # This should be prior to the first training step
+            losses = self._compute_average_losses(model)
+            for key, value in losses.items():
+                self.run.log_scalar(
+                    f'{self.prefix}.{key}',
+                    value,
+                    self.iterations
+                )
+
+    def on_epoch_end(self, model, optimizer, **kwargs):
+        """Score evaluation metrics at end of epoch."""
+        losses = self._compute_average_losses(model)
+        for key, value in losses.items():
+            self.run.log_scalar(
+                f'{self.prefix}.{key}',
+                value,
+                self.iterations
+            )
--- exp/train_model.py
+++ exp/train_model.py
@@ -6,10 +6,11 @@ from sacred.utils import apply_backspaces_and_linefeeds
 import torch
 
 from src.callbacks import SaveReconstructedImages, Progressbar
+from src.datasets.splitting import split_validation
 from src.training import TrainingLoop
 from src.visualization import plot_losses
 
-from .callbacks import LogTrainingLoss
+from .callbacks import LogDatasetLoss, LogTrainingLoss
 from .ingredients import model as model_config
 from .ingredients import dataset as dataset_config
 
@@ -30,13 +31,16 @@ def cfg():
     val_frequency = 4
 
 @EXP.automain
-def train(n_epochs, batch_size, learning_rate, weight_decay, val_size, val_frequency, _run, _log,
-          _seed):
+def train(n_epochs, batch_size, learning_rate, weight_decay, val_size,
+          val_frequency, _run, _log, _seed, _rnd):
     """Sacred wrapped function to run training of model."""
     torch.manual_seed(_seed)
     # Get data, sacred does some magic here so we need to hush the linter
     # pylint: disable=E1120,E1123
     dataset = dataset_config.get_instance(train=True)
+    train_dataset, validation_dataset = split_validation(
+        dataset, val_size, _rnd)
+    test_dataset = dataset_config.get_instance(train=False)
 
     # Get model, sacred does some magic here so we need to hush the linter
     # pylint: disable=E1120
@@ -44,6 +48,8 @@ def train(n_epochs, batch_size, learning_rate, weight_decay, val_size, val_frequ
 
     callbacks = [
         LogTrainingLoss(_run),
+        LogDatasetLoss('validation', validation_dataset, _run, batch_size),
+        LogDatasetLoss('testing', test_dataset, _run, batch_size),
         Progressbar(print_loss_components=True)
     ]
 
@@ -59,7 +65,7 @@ def train(n_epochs, batch_size, learning_rate, weight_decay, val_size, val_frequ
 
     training_loop = TrainingLoop(
         model, dataset, n_epochs, batch_size, learning_rate, weight_decay,
-        val_size, val_frequency, callbacks
+        callbacks
     )
     # Run training
     training_loop()
