94df1d50775b162fe1388cd353004915571e0ca7
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-05-16 17:58:22 +0200

    Extended Hausdorff distance calculation

--- scripts/hausdorff_subsampling.py
+++ scripts/hausdorff_subsampling.py
@@ -5,6 +5,7 @@
 
 
 import numpy as np
+import sys
 
 
 def diameter(X):
@@ -19,6 +20,37 @@ def diameter(X):
     return np.amax(D)
 
 
+def hausdorff_distance(X, Y):
+    '''
+    Calculates the Hausdorff distance between two finite metric spaces,
+    i.e. two finite point clouds.
+    '''
+
+    d_x_Y = 0
+
+    for x in X:
+        d_x_y = sys.float_info.max 
+        for y in Y:
+            d = np.linalg.norm(x - y, ord=None)
+            d_x_y = min(d_x_y, d)
+
+        d_x_Y = max(d_x_Y, d_x_y)
+
+    d_y_X = 0
+
+    for y in Y:
+        d_y_x = sys.float_info.max 
+        for x in X:
+            d = np.linalg.norm(x - y, ord=None)
+            d_y_x = min(d_y_x, d)
+
+        d_y_X = max(d_y_X, d_y_x)
+
+    print(d_x_Y, d_y_X)
+
+    return max(d_x_Y, d_y_X)
+
+
 if __name__ == '__main__':
 
     np.random.seed(42)
@@ -29,9 +61,10 @@ if __name__ == '__main__':
     m = 10
 
     X = np.random.normal(size=(n_points, d))
-
-    print(diameter(X))
-    np.savetxt('/tmp/X.txt', X)
+    X_diam = diameter(X)
 
     for _ in range(n_subsamples):
         Y = X[np.random.choice(X.shape[0], m)]
+        Y_diam = diameter(Y)
+
+        print(hausdorff_distance(X, Y))
