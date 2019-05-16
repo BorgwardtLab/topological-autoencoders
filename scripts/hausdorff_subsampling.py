14ad4f343c4cdd03325e0e62306ed442f5b59fc2
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-05-16 17:22:11 +0200

    Diameter calculation

--- scripts/hausdorff_subsampling.py
+++ scripts/hausdorff_subsampling.py
@@ -7,6 +7,18 @@
 import numpy as np
 
 
+def diameter(X):
+    '''
+    Calculates the diameter of a given set of points and returns it.
+    '''
+
+    # Calculate pairwise distance matrix of all points; this is required
+    # as a prerequisite of the diameter calculation.
+    D = np.sum((X[None, :] - X[:, None])**2, -1)**0.5
+
+    return np.amax(D)
+
+
 if __name__ == '__main__':
 
     np.random.seed(42)
@@ -17,6 +29,9 @@ if __name__ == '__main__':
     m = 10
 
     X = np.random.normal(size=(n_points, d))
+
+    print(diameter(X))
+    np.savetxt('/tmp/X.txt', X)
 
     for _ in range(n_subsamples):
         Y = X[np.random.choice(X.shape[0], m)]
