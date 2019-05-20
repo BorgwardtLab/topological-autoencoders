271120d34ff3aa0df01ff3d616d35833171a7394
Max Horn <max.horn@bsse.ethz.ch>
2019-05-20 22:08:01 +0200

    Added competitors on real world datasets

--- scripts/create_real_world_hyperparameter_search.sh
+++ scripts/create_real_world_hyperparameter_search.sh
@@ -2,6 +2,7 @@
 
 ae_models=(Vanilla TopoRegEdgeSymmetric)
 ae_models_cycles=(model.TopoReg model.TopoRegSorted model.TopoRegEdgeSymmetric)
+competitor_methods=(PCA TSNE Isomap UMAP)
 output_pattern='experiments/hyperparameter_search/real_world/{dataset}/{model}.json'
 output_pattern_cycle='experiments/hyperparameter_search/real_world/{dataset}/{model}-cycle.json'
 
@@ -20,3 +21,10 @@ python scripts/configs_from_product.py exp.hyperparameter_search \
   --name dummy --set overrides.model__parameters__autoencoder_model=ConvolutionalAutoencoder_2D \
   --name dummy2 --set overrides.model__parameters__toposig_kwargs__use_cycles=True \
   --output-pattern ${output_pattern_cycle}
+
+
+python scripts/configs_from_product.py exp.hyperparameter_search \
+  --name model \
+  --set ${competitor_methods[*]} \
+  --name dataset --set MNIST FashionMNIST \
+  --output-pattern ${output_pattern}
