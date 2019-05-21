edbed43a6d415d5f1c71c48a4ae80105062b3635
Max Horn <max.horn@bsse.ethz.ch>
2019-05-21 21:54:34 +0200

    Added dimred hypersearch settings and configs

--- scripts/create_simulated_hyperparameter_search.sh
+++ scripts/create_simulated_hyperparameter_search.sh
@@ -4,14 +4,16 @@ ae_models=(Vanilla TopoRegEdgeSymmetric)
 ae_models_cycles=(TopoRegEdgeSymmetric)
 competitor_methods=(PCA TSNE Isomap UMAP)
 output_pattern='experiments/hyperparameter_search/dimensionality_reduction/{dataset}/{model}.json'
+output_pattern_vae='experiments/hyperparameter_search/dimensionality_reduction/{dataset}/VAE-{model}.json'
 output_pattern_cycle='experiments/hyperparameter_search/dimensionality_reduction/{dataset}/{model}-cycle.json'
+output_pattern_vae_cycle='experiments/hyperparameter_search/dimensionality_reduction/{dataset}/VAE-{model}-cycle.json'
 
-
+# Autoencoder based
 python scripts/configs_from_product.py exp.hyperparameter_search \
   --name model \
   --set ${ae_models[*]} \
   --name dataset  \
-  --set SCurve SwissRoll \
+  --set SCurve \
   --name dummy --set overrides.model__parameters__autoencoder_model=MLPAutoencoder \
   --output-pattern ${output_pattern}
 
@@ -23,10 +25,29 @@ python scripts/configs_from_product.py exp.hyperparameter_search \
   --name dummy --set overrides.model__parameters__autoencoder_model=MLPAutoencoder_Spheres \
   --output-pattern ${output_pattern}
 
+# VAE based
+python scripts/configs_from_product.py exp.hyperparameter_search \
+  --name model \
+  --set ${ae_models[*]} \
+  --name dataset  \
+  --set SCurve \
+  --name dummy --set overrides.model__parameters__autoencoder_model=MLPVAE \
+  --output-pattern ${output_pattern_vae}
+
+python scripts/configs_from_product.py exp.hyperparameter_search \
+  --name model \
+  --set ${ae_models[*]} \
+  --name dataset  \
+  --set Spheres \
+  --name dummy --set overrides.model__parameters__autoencoder_model=MLPVAE \
+  --name dummy2 --set overrides.model__parameters__ae_kwargs__input_dim=101 \
+  --output-pattern ${output_pattern_vae}
+
+# Cycles
 python scripts/configs_from_product.py exp.hyperparameter_search \
   --name model \
   --set ${ae_models_cycles[*]} \
-  --name dataset --set SCurve SwissRoll \
+  --name dataset --set SCurve \
   --name dummy --set overrides.model__parameters__autoencoder_model=MLPAutoencoder \
   --name dummy2 --set overrides.model__parameters__toposig_kwargs__use_cycles=True \
   --output-pattern ${output_pattern_cycle}
@@ -38,6 +59,25 @@ python scripts/configs_from_product.py exp.hyperparameter_search \
   --name dummy --set overrides.model__parameters__autoencoder_model=MLPAutoencoder_Spheres \
   --name dummy2 --set overrides.model__parameters__toposig_kwargs__use_cycles=True \
   --output-pattern ${output_pattern_cycle}
+
+# VAECycles SCurve
+python scripts/configs_from_product.py exp.hyperparameter_search \
+  --name model \
+  --set ${ae_models_cycles[*]} \
+  --name dataset --set SCurve \
+  --name dummy --set overrides.model__parameters__autoencoder_model=MLPAutoencoder \
+  --name dummy2 --set overrides.model__parameters__toposig_kwargs__use_cycles=True \
+  --output-pattern ${output_pattern_vae_cycle}
+
+# VEACycles Spheres
+python scripts/configs_from_product.py exp.hyperparameter_search \
+  --name model \
+  --set ${ae_models_cycles[*]} \
+  --name dataset --set Spheres \
+  --name dummy --set overrides.model__parameters__autoencoder_model=MLPVAE \
+  --name dummy2 --set overrides.model__parameters__ae_kwargs__input_dim=101 \
+  --name dummy3 --set overrides.model__parameters__toposig_kwargs__use_cycles=True \
+  --output-pattern ${output_pattern_vae_cycle}
 
 # Competitor
 python scripts/configs_from_product.py exp.hyperparameter_search \
