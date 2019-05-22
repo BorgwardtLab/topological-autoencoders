def9b91243cf5aff540b5d1f243e08e83bf07a65
Max Horn <max.horn@bsse.ethz.ch>
2019-05-22 16:01:54 +0200

    Added some code in case we want to use python for PH computations

--- src/models/approx_based.py
+++ src/models/approx_based.py
@@ -95,9 +95,20 @@ class TopologicalSignatureDistance(nn.Module):
 
         self.match_edges = match_edges
 
+        # if use_cycles:
+        #     use_aleph = True
+        # else:
+        #     if not sort_selected and match_edges is None:
+        #         use_aleph = True
+        #     else:
+        #         use_aleph = False
+
+        # if use_aleph:
+        #     print('Using aleph to compute signatures')
         self.signature_calculator = AlephPersistenHomologyCalculation(
             compute_cycles=use_cycles, sort_selected=sort_selected)
         # else:
+        #     print('Using python to compute signatures')
         #     self.signature_calculator = PersistentHomologyCalculation()
 
     def _get_pairings(self, distances):
