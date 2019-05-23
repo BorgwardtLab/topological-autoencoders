1177b3758190eb5225aea07f1a39ceff67f3c228
mi92 <michael.d.moor@gmail.com>
2019-05-23 11:31:46 +0200

    added harvesting script load_results for gathering results in dict, tested on dummy variables

---
+++ scripts/load_results.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import json
+import glob
+from IPython import embed
+
+#path = '/links/groups/borgwardt/Projects/TopoAE/topologically-constrained-autoencoder/exp_runs/hyperparameter_search/real_world/COIL/Vanilla/model_runs/1/run.json'
+path =  '/links/groups/borgwardt/Projects/TopoAE/topologically-constrained-autoencoder/exp_runs/hyperparameter_search/dimensionality_reduction'  
+#for testing we take a single run.json
+#filelist= [path]
+filelist = glob.glob(path + '/**/run.json', recursive=True)
+print(filelist)
+
+#list of flat dicts 
+results = []
+for filename in filelist:  
+    split = filename.split('/')
+    dataset = split[-6] #TODO: change to -3 and -2 for real results!
+    model = split[-4]
+    
+    with open(filename, 'rb') as f:
+        data = json.load(f)
+    
+    if 'result' not in data.keys():
+        continue
+        
+    result_keys = list(data['result'].keys())
+    #used_keys = [key for key in result_keys if 'test_density_kl_global_' in key]
+    used_keys = [key for key in result_keys if 'mean' in key]
+  
+    #Create dict of results of current experiment (given dataset and model)
+    experiment = {}
+    experiment['dataset'] = dataset
+    experiment['model'] = model
+    
+    #fill eval measures into experiments dict:
+    for key in used_keys:
+        experiment[key] = data['result'][key]
+
+    results.append(experiment)
+ 
+embed() 
+
+
+
+      
+
+ 
+    
