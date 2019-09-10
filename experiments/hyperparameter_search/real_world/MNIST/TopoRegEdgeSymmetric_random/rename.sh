ba5e806420541cf7e4ad3cfe3ee5d022bfa8cdc2
mi92 <michael.d.moor@gmail.com>
2019-09-10 13:27:51 +0200

    moved neurips configs (folder named experiments) to neurips folder

--- experiments/hyperparameter_search/real_world/MNIST/TopoRegEdgeSymmetric_random/rename.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for f in *.json; do pattern=$(echo sed \'s/"seed": [0-9]\+,/"seed": $RANDOM,/g\'); $pattern $f; done
--- experiments/hyperparameter_search/real_world/MNIST/TopoRegEdgeSymmetric_random/set_random_seeds.sh
+++
@@ -1,8 +0,0 @@
-sed -i 's/"seed": [0-9]\+,/"seed": 32726,/g' rep1.json
-sed -i 's/"seed": [0-9]\+,/"seed": 19031,/g' rep2.json
-sed -i 's/"seed": [0-9]\+,/"seed": 4364,/g' rep3.json
-sed -i 's/"seed": [0-9]\+,/"seed": 28626,/g' rep4.json
-sed -i 's/"seed": [0-9]\+,/"seed": 6261,/g' rep5.json
-sed -i 's/"seed": [0-9]\+,/"seed": 10897,/g' rep6.json
-sed -i 's/"seed": [0-9]\+,/"seed": 25049,/g' rep7.json
-sed -i 's/"seed": [0-9]\+,/"seed": 32449,/g' rep8.json
