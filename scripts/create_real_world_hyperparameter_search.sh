f827616665732d02fbc19c938a4630aaac7218c9
mi92 <michael.d.moor@gmail.com>
2019-09-11 14:46:49 +0200

    updated real_world config generation shell script

--- scripts/create_real_world_hyperparameter_search.sh
+++ scripts/create_real_world_hyperparameter_search.sh
@@ -12,15 +12,15 @@ python scripts/configs_from_product.py exp.hyperparameter_search \
   --name dummy --set overrides.model__parameters__autoencoder_model=DeepAE \
   --output-pattern ${output_pattern}
 
-python scripts/configs_from_product.py exp.hyperparameter_search \
-  --name model \
-  --set ${ae_models[*]} \
-  --name dataset --set  COIL \
-  --name dummy --set overrides.model__parameters__autoencoder_model=DeepAE_COIL \
-  --output-pattern ${output_pattern}
+#python scripts/configs_from_product.py exp.hyperparameter_search \
+#  --name model \
+#  --set ${ae_models[*]} \
+#  --name dataset --set  COIL \
+#  --name dummy --set overrides.model__parameters__autoencoder_model=DeepAE_COIL \
+#  --output-pattern ${output_pattern}
 
 python scripts/configs_from_product.py exp.hyperparameter_search \
   --name model \
   --set ${competitor_methods[*]} \
-  --name dataset --set MNIST FashionMNIST COIL \
+  --name dataset --set MNIST FashionMNIST \
   --output-pattern ${output_pattern}
