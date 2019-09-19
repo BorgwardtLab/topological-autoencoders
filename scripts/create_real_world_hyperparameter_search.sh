081e17cfc966d453f07c4e5a9d61e2f17e81217c
mi92 <michael.d.moor@gmail.com>
2019-09-19 16:50:43 +0200

    added VAE to real-world hypersearch config shell script

--- scripts/create_real_world_hyperparameter_search.sh
+++ scripts/create_real_world_hyperparameter_search.sh
@@ -5,6 +5,9 @@ competitor_methods=(PCA TSNE Isomap UMAP)
 output_pattern='experiments/hyperparameter_search/real_world/{dataset}/{model}.json'
 input_dims='[3,32,32]'
 
+output_pattern_vae='experiments/hyperparameter_search/real_world/{dataset}/VAE-{model}.json'
+
+#AE methods:
 python scripts/configs_from_product.py exp.hyperparameter_search \
   --name model \
   --set ${ae_models[*]} \
@@ -20,13 +23,23 @@ python scripts/configs_from_product.py exp.hyperparameter_search \
     --name dummy --set overrides.model__parameters__ae_kwargs__input_dims=${input_dims} \
     --output-pattern ${output_pattern}
 
-#python scripts/configs_from_product.py exp.hyperparameter_search \
-#  --name model \
-#  --set ${ae_models[*]} \
-#  --name dataset --set  COIL \
-#  --name dummy --set overrides.model__parameters__autoencoder_model=DeepAE_COIL \
-#  --output-pattern ${output_pattern}
+#VAE method:
+python scripts/configs_from_product.py exp.hyperparameter_search \
+  --name model \
+  --set Vanilla \
+  --name dataset --set MNIST FashionMNIST \
+  --name dummy --set overrides.model__parameters__autoencoder_model=DeepVAE \
+  --output-pattern ${output_pattern_vae}
 
+python scripts/configs_from_product.py exp.hyperparameter_search \
+    --name model \
+    --set Vanilla \
+    --name dataset --set CIFAR \
+    --name dummy --set overrides.model__parameters__autoencoder_model=DeepVAE \
+    --name dummy --set overrides.model__parameters__ae_kwargs__input_dims=${input_dims} \
+    --output-pattern ${output_pattern_vae}
+
+#Classic, non-deep Baselines: 
 python scripts/configs_from_product.py exp.hyperparameter_search \
   --name model \
   --set ${competitor_methods[*]} \
