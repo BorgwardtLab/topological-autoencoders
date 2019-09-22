bd6b287672a75359d95bd8a55e55abd682d9a04e
mi92 <michael.d.moor@gmail.com>
2019-09-22 13:18:54 +0200

    updated plot batch size script to create suppl plots from hypersearches

--- scripts/plot_batch_sizes.py
+++ scripts/plot_batch_sizes.py
@@ -1,6 +1,12 @@
 """Create Plots from parameter evaluation file"""
 import argparse
 import numpy as np
+
+import seaborn as sns #; sns.set(color_codes=True)
+
+import matplotlib 
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 import pandas as pd
 from IPython import embed
@@ -10,15 +16,53 @@ def main():
     parser.add_argument('--path', required=True, type=str)
 
     args = parser.parse_args()
+
+    marker = "."
     path = args.path + 'parameter_evaluations.csv'
     data = pd.read_csv(path)
     
-    x = data['hyperparam_optimization_objective']
-    y = data['batch_size']
-    plt.scatter(y, x, marker='o')
-    plt.xlabel('batch size')
-    plt.ylabel('Hypersearch Obj (KL_0.1)') 
-    plt.savefig(path + 'plot_batch_size.png') 
+#    fig, ax = plt.subplots(3,1, sharex=True)
+#    x = data['hyperparam_optimization_objective']
+#    y = data['batch_size']
+#    ax[0].scatter(y,x, marker=marker)
+#    #ax[0].set_xlabel('Batch size')
+#    ax[0].set_ylabel('Objective (KL_0.1)') 
+#    
+#    x2 = data['training.loss.topo_error'] 
+#    ax[1].scatter(y,x2, marker=marker)
+#    #ax[1].set_xlabel('Batch size')
+#    ax[1].set_ylabel('Topological loss')
+#
+#    x3 = data['training.loss.autoencoder'] 
+#    ax[2].scatter(y,x3, marker=marker)
+#    ax[2].set_xlabel('Batch size')
+#    ax[2].set_ylabel('Reconstruction loss')
+
+    batch_size = data['batch_size']
+    xmin = np.min(batch_size) - 3
+    xmax = np.max(batch_size) + 3
+    #plt.figure()
+    fig, ax = plt.subplots()
+
+    used = ['batch_size', 'training.loss.autoencoder', 'training.loss.topo_error', 'hyperparam_optimization_objective'] 
+    labels = ['Reconstruction Loss', 'Topological Loss', 'Objective (KL_0.1)']
+    df = data[used]
+    
+    for i in np.arange(1,len(used)):
+        #sns.plt.xlim(35, 100)
+        sns.regplot(x='batch_size', y=used[i], data=df, label=labels[i-1], truncate=False, ax=ax)      
+        ax.set(xlabel="Batch Size", ylabel = "Loss Measures")
+        ax.set_xlim(xmin, xmax)
+        plt.legend()
+    plt.savefig('plots/' + f'plot_batch_size_vs_losses.png') 
+    
+#    sns_plot = sns.pairplot(df)
+#    sns_plot.savefig('plots/test_pairplot.png')
+
+
+
+    #plt.savefig(path + 'plot_batch_size.png') 
+    #plt.savefig('plots/' + 'plot_batch_size.png') 
 
 if __name__ == '__main__':
     main()
